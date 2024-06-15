9d41d4fa0a21285ed100172f1b9d832b0ddab46b
lucleber13 <lucleber13@hotmail.com>
2024-06-15 18:52:48 +0100

    Project created

---
+++ src/test/java/cbcoder/dealerwebapp/DealerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cbcoder.dealerwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
